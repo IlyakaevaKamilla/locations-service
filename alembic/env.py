369579ac6c33deb7902963758c646018f5c9330e
Viktor Balonkin <v.balonkin@gmail.com>
2026-08-23 00:07:04 +0300

    code style

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-from app.db.base import Base  # noqa
+from app.db.base import Base
 from app.db.models import *
 from config import settings as se
 
